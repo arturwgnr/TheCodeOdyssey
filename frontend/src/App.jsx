85119f6e62dc00d43c83e55b6b81ea0bd49cd19a
Artur Wagner <arturwagner99@hotmail.com>
2025-11-15 20:30:59 +0000

    AuthContext

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Artur Wagner</h1>
+      <p>You have literally everything you need in order to succeed!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+
+    if (storedToken) setToken(storedToken);
+    if (storedUser) setUser(JSON.parse(storedUser));
+  }, []);
+
+  function login(token, userData) {
+    setToken(token);
+    setUser(userData);
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(userData));
+  }
+
+  // Logout handler
+  function logout() {
+    setToken(null);
+    setUser(null);
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, setToken, user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
